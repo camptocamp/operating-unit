13b6e25f8006c8b259d0c4fb8f4c8f958986670a
Maksym Yankin <yankinmk@gmail.com>
2024-06-04 13:14:32 +0300

    [MIG] res_partner_operating_unit: Migration to 17.0

--- res_partner_operating_unit/tests/test_res_partner_operating_unit.py
+++ res_partner_operating_unit/tests/test_res_partner_operating_unit.py
@@ -1,51 +1,56 @@
 # © 2017 Niaga Solution - Edi Santoso <repodevs@gmail.com>
 # Copyright (C) 2020 Serpent Consulting Services
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-from odoo.tests import common
+from odoo import Command
+from odoo.exceptions import UserError
+from odoo.tests.common import TransactionCase
 
 
-class TestResPartnerOperatingUnit(common.TransactionCase):
-    def setUp(self):
-        super(TestResPartnerOperatingUnit, self).setUp()
-        self.res_partner_model = self.env["res.partner"]
-        self.res_users_model = self.env["res.users"]
+class TestResPartnerOperatingUnit(TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
+        cls.res_partner_model = cls.env["res.partner"]
+        cls.res_users_model = cls.env["res.users"]
         # Company
-        self.company = self.env.ref("base.main_company")
+        cls.company = cls.env.ref("base.main_company")
         # Main Operating Unit
-        self.ou1 = self.env.ref("operating_unit.main_operating_unit")
+        cls.ou1 = cls.env.ref("operating_unit.main_operating_unit")
         # B2C Operating Unit
-        self.b2c = self.env.ref("operating_unit.b2c_operating_unit")
+        cls.b2c = cls.env.ref("operating_unit.b2c_operating_unit")
 
         # Create User 1 with Main OU
-        self.user1 = self._create_user("user_1", self.company, [self.ou1])
+        cls.user1 = cls._create_user("user_1", cls.company, cls.ou1)
         # Create User 2 with B2C OU
-        self.user2 = self._create_user("user_2", self.company, [self.b2c])
+        cls.user2 = cls._create_user("user_2", cls.company, cls.b2c)
 
         # Create Partner 1 with Main OU
-        self.partner1 = self._create_partner("Test Partner 1", self.ou1)
+        cls.partner1 = cls._create_partner("Test Partner 1", cls.ou1)
 
         # Create Partner 2 with B2C OU
-        self.partner2 = self._create_partner("Test Partner 2", self.b2c)
+        cls.partner2 = cls._create_partner("Test Partner 2", cls.b2c)
 
-    def _create_partner(self, name, operating_unit, context=None):
+    @classmethod
+    def _create_partner(cls, name, operating_unit, context=None):
         """Create a partner."""
-        partner = self.res_partner_model.create(
-            {"name": name, "operating_unit_id": operating_unit.id}
+        partner = cls.res_partner_model.create(
+            {"name": name, "operating_unit_ids": [Command.set(operating_unit.ids)]}
         )
         return partner
 
-    def _create_user(self, login, company, operating_units, context=None):
+    @classmethod
+    def _create_user(cls, login, company, operating_units, context=None):
         """Create a user."""
-        user = self.res_users_model.create(
+        user = cls.res_users_model.create(
             {
                 "name": "Test User",
                 "login": login,
                 "password": "demo",
                 "email": "test@yourcompany.com",
                 "company_id": company.id,
-                "company_ids": [(4, company.id)],
-                "operating_unit_ids": [(4, ou.id) for ou in operating_units],
-                "notification_type": "inbox",
+                "company_ids": [Command.set(company.ids)],
+                "operating_unit_ids": [Command.set(operating_units.ids)],
             }
         )
         return user
@@ -53,5 +58,16 @@ class TestResPartnerOperatingUnit(common.TransactionCase):
     def _update_user(self, user, operating_units, context=None):
         with self.assertRaises(UserError):
             user.write(
-                {"default_operating_unit_id": [(4, ou.id) for ou in operating_units]}
+                {"default_operating_unit_id": [[Command.set(operating_units.ids)]]}
             )
+
+    def test_01_operating_unit(self):
+        """Test Operating Unit."""
+        self.assertEqual(self.user1.default_operating_unit_id, self.ou1)
+        self.assertEqual(self.user2.default_operating_unit_id, self.ou1)
+
+        self._update_user(self.user1, self.b2c)
+        self._update_user(self.user2, self.ou1)
+
+        self.assertEqual(self.partner1.operating_unit_ids, self.ou1)
+        self.assertEqual(self.partner2.operating_unit_ids, self.b2c)
