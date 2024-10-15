c6cd96606c5c5a9c31caf2cf511f299bc8c7a6d5
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2024-10-15 08:29:40 +0200

    fixup! [FIX] crm_operating_unit: warning in logs

--- crm_operating_unit/models/crm_lead.py
+++ crm_operating_unit/models/crm_lead.py
@@ -7,7 +7,6 @@ from odoo import api, fields, models
 class CRMLead(models.Model):
     _inherit = "crm.lead"
 
-
     operating_unit_id = fields.Many2one(
         "operating.unit",
         "Operating Unit",
@@ -17,7 +16,9 @@ class CRMLead(models.Model):
     @api.depends
     def _compute_operating_unit(self):
         default_team = self.env["crm.team"]._get_default_team_id()
-        default_operating_unit = self.env["res.users"].operating_unit_default_get(self._uid)
+        default_operating_unit = self.env["res.users"].operating_unit_default_get(
+            self._uid
+        )
         for record in self:
             team = record.team_id or default_team
             if team.operating_unit_id:
