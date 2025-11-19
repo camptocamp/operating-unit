42a3a1d3f0e67ac63568b445426e255e900bc5ea
jcoux <julien.coux@camptocamp.com>
2025-11-19 11:12:16 +0100

    [MIG] analytic_operating_unit: Migration to 19.0

--- analytic_operating_unit/static/src/components/analytic_distribution.esm.js
+++ analytic_operating_unit/static/src/components/analytic_distribution.esm.js
@@ -7,6 +7,11 @@ patch(AnalyticDistribution.prototype, {
         if ("fields" in res) {
             for (const [key, value] of Object.entries(res.fields)) {
                 if (key.startsWith("x_plan") && "domain" in value) {
+                    // NB: The JS part here is **generic** and doesn't work alone.
+                    // The `operating_unit_id` field is provided by the models that use an analytic distribution.
+                    // For example,
+                    // the `account.move.line` will have the field (defined in `account_operating_unit` module),
+                    // and then, this JS will be applied in that context.
                     if (
                         "operating_unit_id" in this.props.record.data &&
                         this.props.record.data.operating_unit_id
